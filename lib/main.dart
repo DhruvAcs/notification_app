3d56e9453aee193ebf26af369721181c2015cdd5
Dhruv <amind@bxscience.edu>
2022-05-26 08:31:45 -0400

    dark mode incomplete

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:notification_app/Auth/main_page.dart';
 import 'pages/login_page.dart';
@@ -8,24 +10,35 @@ Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     // Replace with actual values
-    options: FirebaseOptions(
+    options: const FirebaseOptions(
       apiKey: "AIzaSyDRatQVo_t6wROVtCLcxkhgtPS27i3VePQ",
       appId: "1:546133077542:android:746b23fd482678c8fcc0e7",
       messagingSenderId: "546133077542",
       projectId: "schedula-cd37d",
     ),
   );
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({Key? key}) : super(key: key);
+
+  static final ValueNotifier<ThemeMode> themeNotifier = ValueNotifier(ThemeMode.light);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainPage(),
+    return ValueListenableBuilder<ThemeMode>(
+        valueListenable: themeNotifier,
+        builder: (_, ThemeMode currentMode, __) {
+          return MaterialApp(
+            darkTheme: ThemeData.dark(),
+            theme: ThemeData.light(),
+            themeMode: currentMode,
+            debugShowCheckedModeBanner: false,
+            home: const MainPage(),
+          );
+        },
     );
   }
 }
--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,7 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:settings_ui/settings_ui.dart';
-
+import 'package:notification_app/main.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -18,20 +20,58 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body:  SettingsList(
+        darkTheme: const SettingsThemeData(settingsListBackground: Colors.white),
+        lightTheme: const SettingsThemeData(settingsListBackground: Colors.white),
         sections: [
           SettingsSection(
-            title: Text('Common'),
+            title: const Text('General'),
             tiles: <SettingsTile>[
               SettingsTile.navigation(
-                leading: Icon(Icons.language),
-                title: Text('Language'),
-                value: Text('English'),
+                leading: const Icon(Icons.language),
+                title: const Text('Language'),
+                value: const Text('English'),
+              ),
+              SettingsTile.switchTile(
+                onToggle: (value) {
+                  log(value.toString());
+                  log(MyApp.themeNotifier.value.toString());
+
+                  if (value) {
+                    return MyApp.themeNotifier.value = ThemeMode.dark;
+                  }
+                  MyApp.themeNotifier.value = ThemeMode.light;
+                  setState(() {
+                  });
+                },
+                initialValue: MyApp.themeNotifier.value == ThemeMode.light ? false : true,
+                leading: const Icon(Icons.dark_mode),
+                title: const Text('Dark Mode'),
+              ),
+              SettingsTile.navigation(
+                leading: const Icon(Icons.person),
+                title: const Text('Teacher select'),
+                value: const Text('English'),
+              ),
+              SettingsTile.navigation(
+                leading: const Icon(Icons.web),
+                title: const Text('School website'),
+                value: const Text('School Name'),
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: const Text('Account'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(Icons.search),
+                title: const Text('Password'),
+                value: const Text('*********'),
               ),
               SettingsTile.switchTile(
                 onToggle: (value) {},
                 initialValue: true,
-                leading: Icon(Icons.format_paint),
-                title: Text('Enable custom theme'),
+                leading: const Icon(Icons.person),
+                title: const Text('Teacher Select'),
               ),
             ],
           ),
